6829037273d1f45d5bae1ac69e27a5d23e4904a5
mostafazayedd <mostafa.zayed@aiu.edu.eg>
2026-05-15 20:57:02 +0300

    feat: add Prometheus metrics endpoint /metrics to both services

--- services/central-logging/src/app.js
+++ services/central-logging/src/app.js
@@ -22,3 +22,28 @@ app.get("/ready", (req, res) => {
 app.use("/logs", logsRouter);
 
 module.exports = app;
+const client = require('prom-client');
+
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+const httpRequestDurationMicroseconds = new client.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10]
+});
+register.registerMetric(httpRequestDurationMicroseconds);
+
+app.use((req, res, next) => {
+  const end = httpRequestDurationMicroseconds.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.route?.path || req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
